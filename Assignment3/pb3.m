99289d2ca6ca94709851edc5419d4211e480c2e4
Andrew Van <vanandrew@wustl.edu>
2019-03-22 13:09:43 -0500

    update

--- Assignment3/pb3.m
+++ Assignment3/pb3.m
@@ -2,4 +2,22 @@ clc;
 clear;
 close all;
 
-[f,g,H] = objectiveFunction(x);
+step = 1e-2;
+x = -1:step:1;
+y = -1:step:1;
+[xx, yy] = meshgrid(x,y);
+nx = numel(x);
+ny = numel(y);
+f = zeros(nx,ny);
+g = zeros(nx,ny,2);
+H = zeros(nx,ny,2,2);
+for i=1:nx
+    for j=1:ny
+        [f(i,j),g(i,j,:),H(i,j,:,:)] = objectiveFunction([x(i);y(j)]);
+    end
+end
+[gy, gx] = gradient(f,step);
+a = g(:,:,1) - gx; 
+b = g(:,:,2) - gy;
+quiver(xx,yy,a,b);
+
