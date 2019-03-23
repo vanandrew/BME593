9fe218ca9e4fe002aacf2cb3a5ad0df7554a7955
Andrew Van <vanandrew@wustl.edu>
2019-03-23 00:46:31 -0500

    do problem3

--- Assignment3/pb3.m
+++ Assignment3/pb3.m
@@ -2,22 +2,48 @@ clc;
 clear;
 close all;
 
-step = 1e-2;
-x = -1:step:1;
-y = -1:step:1;
-[xx, yy] = meshgrid(x,y);
-nx = numel(x);
-ny = numel(y);
-f = zeros(nx,ny);
-g = zeros(nx,ny,2);
-H = zeros(nx,ny,2,2);
-for i=1:nx
-    for j=1:ny
-        [f(i,j),g(i,j,:),H(i,j,:,:)] = objectiveFunction([x(i);y(j)]);
-    end
-end
-[gy, gx] = gradient(f,step);
-a = g(:,:,1) - gx; 
-b = g(:,:,2) - gy;
-quiver(xx,yy,a,b);
+% settings
+n = 250;
+iterations = 50;
+backtrack_iter = 100;
+rtol = 10^-8;
+c = 1e-5;
 
+% set initial x
+x = zeros(n,1);
+
+for i=1:iterations
+    % Get f,g and h
+    [f,g,H] = objectiveFunction(x);
+    
+    % save initital gradient
+    if i == 1
+       g0 = g; 
+    end
+    
+    % break if <= tolerance
+    if norm(g)/norm(g0) <= rtol
+        break;
+    end
+    
+    % get pk using cg_steihaug
+    %eta = 0.5;
+    %eta = min([0.5, sqrt(norm(g)/norm(g0))]);
+    eta = min([0.5, norm(g)/norm(g0)]);
+    [pk, si] = cg_steihaug(H,-g,1000,eta,zeros(n,1));
+    
+    % do armijo backtracking
+    alpha = 1;
+    for j=1:backtrack_iter
+        [fprop,~,~] = objectiveFunction(x+alpha*pk);
+        if fprop <= f + c*alpha*g'*pk
+            break;
+        else
+            alpha = alpha/2;
+        end
+    end
+    
+    % update x
+    x = x + alpha*pk;
+    plot(x);
+end
